f0edb2368487bae67de566006a02b09efb5c46f6
Vitorr17 <147094812+Vitorr17@users.noreply.github.com>
2023-10-25 15:26:38 -0300

    atualizar

---
+++ Consutorio/Models/Consulta.cs
@@ -0,0 +1,9 @@
+﻿namespace consutorio.Models
+{
+    public class Consulta
+    {
+        public int id { get; set; } 
+        public string Name { get; set; }    
+        public string Sobrenome { get; set; }    
+    }
+}
