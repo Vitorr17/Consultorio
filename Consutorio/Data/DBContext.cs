8fa8bbbe16cae62b3f2afbabc56bf1f7184e59c1
Vitorr17 <147094812+Vitorr17@users.noreply.github.com>
2023-10-27 17:05:46 -0300

    atualizar

---
+++ Consutorio/Data/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using consutorio.Models;
+
+namespace consutorio.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext (DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<consutorio.Models.Consulta> Consulta { get; set; } = default!;
+    }
+}
